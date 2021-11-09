8383e40c355493fdaf4002a09f23435375965c6a
Mars <martinianol@outlook.com>
2021-11-09 19:17:32 -0300

    Finished mobile version

--- time-tracking-dashboard-main/model/model.js
+++ time-tracking-dashboard-main/model/model.js
@@ -3,6 +3,7 @@ const data = require('./data.json');
 const dataDaily = data.map(element => {
   return {
     title: element.title,
+    type: 'day',
     icon: element.icon,
     current: element.timeframes.daily.current,
     previous: element.timeframes.daily.previous
@@ -11,6 +12,7 @@ const dataDaily = data.map(element => {
 const dataWeekly = data.map(element => {
   return {
     title: element.title,
+    type: 'week',
     icon: element.icon,
     current: element.timeframes.weekly.current,
     previous: element.timeframes.weekly.previous
@@ -19,6 +21,7 @@ const dataWeekly = data.map(element => {
 const dataMonthly = data.map(element => {
   return {
     title: element.title,
+    type: 'month',
     icon: element.icon,
     current: element.timeframes.monthly.current,
     previous: element.timeframes.monthly.previous
