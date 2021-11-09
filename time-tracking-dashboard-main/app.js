71741290ef36a5a478b9fabb55cdba5b360000be
Mars <martinianol@outlook.com>
2021-11-09 13:48:15 -0300

    EJS implemented

--- time-tracking-dashboard-main/app.js
+++ time-tracking-dashboard-main/app.js
@@ -10,10 +10,14 @@ app.use('/public', express.static('public'));
 //Allow CORS
 app.use(cors());
 
-app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
-});
 
+//Configuracion del template engine//
+app.set('views', path.join(__dirname, 'views')) // indica al template engine donde buscar las vistas//
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res, next) => {
+  res.render('index.ejs')
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
